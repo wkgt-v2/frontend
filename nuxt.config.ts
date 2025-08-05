1e077c440e79d296338ebe5bdf52d2dcd73ca468
RifkyLTF <rifky.luthfan@gmail.com>
2025-08-05 19:17:01 +0700

    chore: disable dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
     "@nuxt/ui",
     "@nuxt/eslint"
   ],
-
   css: ["~/assets/css/main.css"],
-
+  ui: {
+    colorMode: false,
+  },
   compatibilityDate: "2025-07-16"
 });
