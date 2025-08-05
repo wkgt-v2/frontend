38010944ecac9bfd9cc911011f79749baf459f20
RifkyLTF <rifky.luthfan@gmail.com>
2025-08-05 19:23:32 +0700

    chore: adjust picsum resolution

--- app/components/landing/banner.vue
+++ app/components/landing/banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <img src="https://picsum.photos/960/196?random=6" class="aspect-[240/49] w-full object-contain rounded-lg md:rounded-2xl">
+    <img src="https://picsum.photos/600/100?random=6" class="aspect-[240/49] w-full object-contain rounded-lg md:rounded-2xl">
   </div>
 </template>
 
--- app/components/landing/carousel.vue
+++ app/components/landing/carousel.vue
@@ -15,10 +15,10 @@
 
 <script setup lang="ts">
 const items = [
-  "https://picsum.photos/1200?random=1",
-  "https://picsum.photos/1200?random=2",
-  "https://picsum.photos/1200?random=3",
-  "https://picsum.photos/1200?random=4",
-  "https://picsum.photos/1200?random=5",
+  "https://picsum.photos/600?random=1",
+  "https://picsum.photos/600?random=2",
+  "https://picsum.photos/600?random=3",
+  "https://picsum.photos/600?random=4",
+  "https://picsum.photos/600?random=5",
 ]
 </script>
