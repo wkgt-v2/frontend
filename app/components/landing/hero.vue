fa139f7a0af556e45934799a16bb350da0b7d196
RifkyLTF <rifky.luthfan@gmail.com>
2025-08-17 18:46:01 +0700

    style: landing page hero

--- app/components/landing/hero.vue
+++ app/components/landing/hero.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="relative container flex items-center gap-6 -mt-16 min-h-dvh">
+  <div class="relative container flex items-center -mt-16 min-h-dvh">
     <Radial class="absolute -top-1/3 -left-1/4 size-[960px] -z-10" />
     <Radial class="absolute -top-1/5 -right-1/5 size-[960px] -z-10" />
     <div class="absolute inset-0 border-b border-primary overflow-hidden -z-10">
       <Radial class="absolute bottom-0 left-1/2 translate-y-[55%] -translate-x-1/2 size-[960px] -z-10" />
     </div>
-    <div class="shrink-0 space-y-6 w-full max-w-[514px] py-32 text-tone">
-      <h1 class="text-5xl font-semibold [&_span]:text-primary">
-        Solusi <span>Distribusi Perangkat Keras Komputer</span> untuk Bisnis Anda
-      </h1>
-      <p>
-        Berpengalaman lebih dari satu dekade dalam menyediakan produk berkualitas dan layanan menyeluruh, kami menjadi mitra andal bagi vendor dan klien di seluruh Indonesia.
-      </p>
+    <div class="grid grid-cols-2 items-center gap-6 py-32">
+      <div class="shrink-0 space-y-6 max-w-[514px] text-tone">
+        <h1 class="text-5xl font-semibold [&_span]:text-primary">
+          Solusi <span>Distribusi Perangkat Keras Komputer</span> untuk Bisnis Anda
+        </h1>
+        <p class="text-tone">
+          Berpengalaman lebih dari satu dekade dalam menyediakan produk berkualitas dan layanan menyeluruh, kami menjadi mitra andal bagi vendor dan klien di seluruh Indonesia.
+        </p>
+      </div>
+      <img
+        src="/assets/images/illustrations/pc-parts.png"
+        alt="PC Parts"
+        width="678"
+        height="680"
+        class="w-full"
+        loading="lazy"
+      />
     </div>
-    <img
-      src="/assets/images/illustrations/pc-parts.png"
-      alt="PC Parts"
-      width="678"
-      height="680"
-      loading="lazy"
-    />
   </div>
 </template>
