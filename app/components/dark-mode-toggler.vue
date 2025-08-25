65621c64a7cbff38d7e1470c16c7ed4c5c9a3b78
RifkyLTF <rifky.luthfan@gmail.com>
2025-08-25 09:16:41 +0700

    style: dark-mode toggler

--- app/components/dark-mode-toggler.vue
+++ app/components/dark-mode-toggler.vue
@@ -1,9 +1,15 @@
 <template>
   <UIcon
-    name="i-material-symbols-dark-mode-outline"
-    class="size-6 text-tone cursor-pointer"
+    name="i-material-symbols:light-mode-outline"
+    class="hidden size-6 text-tone cursor-pointer dark:flex"
     mode="svg"
-    @click="isDark = !isDark"
+    @click="isDark = false"
+  />
+  <UIcon
+    name="i-material-symbols:dark-mode-outline"
+    class="flex size-6 text-tone cursor-pointer dark:hidden"
+    mode="svg"
+    @click="isDark = true"
   />
 </template>
 
