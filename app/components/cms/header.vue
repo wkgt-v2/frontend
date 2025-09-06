14c0e9f654ac8054b0f2a8d522b6f063f46720c8
RifkyLTF <rifky.luthfan@gmail.com>
2025-09-06 19:12:55 +0700

    style: hide cms title

--- app/components/cms/header.vue
+++ app/components/cms/header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex items-center justify-between h-[72px] glass px-6 border-b border-accent">
-    <h4 class="text-xl text-tone font-semibold capitalize">
+    <h4 class="text-xl text-tone font-semibold capitalize opacity-0">
       {{ title }}
     </h4>
     <DarkModeToggler />
