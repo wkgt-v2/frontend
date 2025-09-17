e9f0e44a7ba9f63e48d8831c5b8099643dcdbbf2
RifkyLTF <rifky.luthfan@gmail.com>
2025-09-17 20:47:56 +0700

    fix: runtime error

--- app/components/dark-mode-toggler.vue
+++ app/components/dark-mode-toggler.vue
@@ -1,18 +1,20 @@
 <template>
-  <UIcon
-    v-if="isDark"
-    name="i-material-symbols:light-mode-outline"
-    class="hidden size-6 text-tone cursor-pointer dark:flex"
-    mode="svg"
-    @click="isDark = false"
-  />
-  <UIcon
-    v-else
-    name="i-material-symbols:dark-mode-outline"
-    class="flex size-6 text-tone cursor-pointer dark:hidden"
-    mode="svg"
-    @click="isDark = true"
-  />
+  <ClientOnly>
+    <UIcon
+      v-if="isDark"
+      name="i-material-symbols:light-mode-outline"
+      class="hidden size-6 text-tone cursor-pointer dark:flex"
+      mode="svg"
+      @click="isDark = false"
+    />
+    <UIcon
+      v-else
+      name="i-material-symbols:dark-mode-outline"
+      class="flex size-6 text-tone cursor-pointer dark:hidden"
+      mode="svg"
+      @click="isDark = true"
+    />
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
