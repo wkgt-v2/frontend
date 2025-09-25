98b5034d7ef34b562482b16bae51a2d5b752ec59
RifkyLTF <rifky.luthfan@gmail.com>
2025-09-25 15:19:19 +0700

    feat: translation for privacy policy & terms of services

---
+++ app/components/locale/en/privacy-policy.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="container space-y-8 py-16">
+    <h1 class="text-3xl text-primary text-center font-semibold">
+      Privacy Policy
+    </h1>
+    <div class="space-y-4 text-tone">
+      <p>
+        Welcome to the home of Wahana!
+      </p>
+      <p>
+        www.wahana-online.com is a site owned by PT. Wahana Karya Guna Teknologi and is fully operated by PT. Wahana Karya Guna Teknologi.
+      </p>
+      <p>
+        The following privacy policy explains the terms of use of the wahana-online site as listed in the “Terms & Conditions” and other information contained on the wahana-online site. How wahana-online collects, stores, uses, discloses User data and at the same time protects the personal information of Users obtained and collected from Users consisting of User data, which is used when registering, creating assets, and using services on the wahana-online site.
+      </p>
+      <p>
+        Please note that it is important for Users to understand this privacy policy together with other applicable information that forms the basis for managing Users' personal data. This privacy policy is published to help Users understand how wahana-online collects, processes, uses, and/or discloses personal data that Users have provided and entrusted to wahana-online and/or that wahana-online holds about the Users, both now and in the future, and to help Users make an informed decision before providing personal data to wahana-online. By continuing to use wahana-online services, Users are deemed to agree to all privacy policies stated on the wahana-online site including any amendments. If Users object and do not permit the processing of their personal data as described in this privacy policy, it is not recommended to use wahana-online services or access the wahana-online site.
+      </p>
+      <p>
+        Wahana-online has the full right to manage and make changes to the privacy policy at any time without the User's consent.
+      </p>
+      <p>This privacy policy covers the following matters:</p>
+      <ol class="space-y-6 list-decimal pl-7">
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Personal Information
+          </h5>
+          <p>
+            Personal information is any form of information recorded in material or immaterial form that represents a person's identity and can be directly observed or reasonably recognized and can be positively identified by an entity collecting that information, or when compared and/or combined with other information, may identify a person directly or indirectly, and may be accessible to authorities in case of issues.
+          </p>
+          <span>
+            While Users use the services or access the wahana-online site, wahana-online may collect Users' personal Information, which includes but is not limited to:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Identity data, such as name, gender, profile picture and date of birth, contact data for phone, shipping and email address;
+            </li>
+            <li>
+              Account data, such as bank account and non-bank account information;
+            </li>
+            <li>
+              Transaction data, such as order details or asset purchase history or transaction history that Users make while transacting on wahana-online;
+            </li>
+            <li>
+              Profile data, such as username and password, purchases, orders or preference interests;
+            </li>
+            <li>
+              Technical data, such as Internet Protocol (IP) address, login data, time and timezone settings, browser plug-in types and versions, browser types and versions, operating system, device identifiers and all related information about the device used to access and/or transact on the wahana-online site;
+            </li>
+            <li>
+              Marketing and correspondence data, relating to Users' preferences in receiving and/or engaging with marketing from and/or on behalf of wahana-online and/or third parties and preferences for communication mechanisms and/or tools.
+            </li>
+            <li>
+              Usage data, such as information on how Users use the Buruan site, assets and/or services;
+            </li>
+            <li>
+              Location, meaning the method by which Users capture and provide location in the form of photos and/or videos and upload them to the wahana-online site;
+            </li>
+            <li>
+              Biometric, meaning voice files when Users use facial features and other body part features and/or any device feature and upload and/or use them on the wahana-online site;
+            </li>
+          </ol>
+          <p>
+            Users agree not to provide any inaccurate or misleading information, and Users agree to notify wahana-online of any inaccuracies and/or changes to that information.<br />
+            Buruan reserves the discretion to require further documentation to verify information submitted by Users.
+          </p>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Age-Related Information
+          </h5>
+          <p>
+            The wahana-online service is not intended for children under 14 years of age. Wahana-online does not intentionally collect or store any personal or non-personal information from anyone under 14 years old and any part of the wahana-online site or services is not directed to children under 14. All parents or legal guardians are strongly advised not to allow children in their care to submit personal data to wahana-online. In the event personal data of children under 14 in your care are disclosed to wahana-online, you hereby consent to the use of that child's personal data and agree to be bound by this Privacy Policy on behalf of that child. Wahana-online will deactivate any account used by such children and will remove and/or delete any personal data submitted without the consent of a parent or legal guardian for all children under 14.
+          </p>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Use and Utilization of User Data
+          </h5>
+          <span>
+            Wahana-online may and is permitted to use all or part of the data and information obtained from Users as mentioned in the previous section for the following purposes:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Identifying and registering Users to verify, manage or deactivate User accounts;
+            </li>
+            <li>
+              Verifying and/or facilitating parts or all of the aspects required before wahana-online begins providing services to Users and/or before registering Users, including the “Know Your Customer” process;
+            </li>
+            <li>
+              Providing User Service facilities to carry out User instructions, handle or respond to any inquiries made by Users and/or on behalf of Users.
+            </li>
+            <li>
+              Enabling wahana-online to provide, process and facilitate orders and payment transactions as well as to send communications related to marketing, promotions, surveys and/or special offers both directly and indirectly;
+            </li>
+            <li>
+              Communicating with Users and sending information in connection with the use of services including but not limited to updates and support that may be required from time to time to ensure the smooth operation of wahana-online services and notifying Users about updates to the wahana-online site and/or changes to the services provided;
+            </li>
+            <li>
+              Analyzing and monitoring user activity, demographics and behavior including usage habits of the various available services and using information obtained from Users for data collection, research, analysis, product development and testing to improve security and convenience of services on the site, and for feature development in both the short and long term;
+            </li>
+            <li>
+              Monitoring transactions suspected of containing violations of Buruan's internal Terms and Conditions and/or applicable laws, and taking necessary follow-up actions where in certain circumstances, wahana-online will use or disclose User data for law enforcement purposes or to comply with legal requirements, including in the event of disputes or legal proceedings between Users and wahana-online or with third parties;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Disclosure of Users' Personal Data
+          </h5>
+          <span>
+            Wahana-online guarantees that there will be no sale, transfer, leakage or lending of Users' personal data to other parties without the User's permission and/or knowledge, except in the following situations:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              When disclosure of User data to other parties is necessary to help wahana-online provide services within the wahana-online ecosystem and to process all User activities on the wahana-online site, including but not limited to transaction processing, payment verification, and providing data and information to business partners in relation to the use of those partners' services, including other application or platform services, API integrations where wahana-online has cooperated with such parties for various service needs on the wahana-online site;
+            </li>
+            <li>
+              Sharing User data and information with subsidiaries and affiliates to help maximize and/or provide services and data processing for and on behalf of wahana-online;
+            </li>
+            <li>
+              Buruan may provide relevant information to banking services, vendors, consultants, marketing partners, research companies and similar service providers;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Agreement on Data Usage
+          </h5>
+          <span>
+            Wahana-online may receive Users' personal data in the following situations:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Users use features available on the wahana-online site;
+            </li>
+            <li>
+              Users create an account on the Buruan site and/or update an account on the wahana-online site including username, address, email address, phone number, password, and others;
+            </li>
+            <li>
+              Users register to become a provider or purchase an asset available on the wahana-online site and record and/or upload user-generated content to the wahana-online site;
+            </li>
+            <li>
+              Users participate in an activity or campaign on the wahana-online site, take part in surveys, competitions or promotions;
+            </li>
+            <li>
+              Users interact with wahana-online through external services or applications;
+            </li>
+            <li>
+              Users interact with wahana-online offline, including when Users interact with wahana-online staff offline;
+            </li>
+            <li>
+              Users use the chat function on the wahana-online site;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Cookies
+          </h5>
+          <span>
+            Cookies are small data storage on a computer or other device when someone accesses a site. Matters related to Cookies can be described as follows:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Cookies are identifiers transferred to a computer or device that allow us to recognize that computer or device and inform Buruan how and when the service is used or visited by the user, and to track activity on the site. Wahana-online may link cookie information to personal data. Cookies are also linked to transaction information and are used to deliver content tailored to interests and to monitor the use of the wahana-online site;
+            </li>
+            <li>
+              Users will automatically receive cookies when the computer or device is used or when browsing the wahana-online site. Users may choose to refuse, select, or modify cookies through web or browser preference settings on the device or computer;
+            </li>
+            <li>
+              Wahana-online uses Google Analytics, where data from the use of that feature including IP address, device type, and others is used by Buruan for the development of the Buruan ecosystem and for advertising; however, Users may opt out of this feature by downloading the Google Analytics Opt-Out Add-on;
+            </li>
+            <li>
+              Cookie usage can be controlled by Users via their web browser preferences by modifying or choosing to refuse the use of cookies, but this may result in suboptimal use of features on the wahana-online site;
+            </li>
+            <li>
+              Wahana-online may use and provide User data obtained from cookie usage to third parties, such as location data or email addresses used for advertising purposes, including but not limited to promotional and advertising needs where such data cannot be personally identified;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Protection of Personal Data
+          </h5>
+          <span>
+            Wahana-online is fully committed to implementing various security measures and procedures to protect data and strives to ensure Users' personal data in the system. Personal data is stored within a highly secure network and can only be accessed by specific admins with special access rights to the system, and their number is kept to a minimum. However, there is no absolute guarantee of digital data security. Buruan stores personal data in accordance with Privacy Laws and/or other applicable laws. That is, Buruan will destroy Users' personal data when it is reasonably considered that:
+          </span>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              The purpose for which the personal data was collected is no longer served by retaining that personal data;
+            </li>
+            <li>
+              There is no legal order to retain that personal data. If a User stops using wahana-online services, or the User's permission to use the service is terminated or withdrawn, wahana-online may continue to store, use and/or disclose the User's personal data in accordance with this Privacy Policy and wahana-online's obligations under applicable privacy law subject to the laws of the Republic of Indonesia. Wahana-online may securely dispose of Users' personal data without prior notice;
+            </li>
+            <li>
+              The storage of data is no longer necessary for any legal or business purpose;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Data and Information Storage and Deletion
+          </h5>
+          <b>
+            Wahana-online is committed to adopting and practicing the following core principles related to data storage and deletion:
+          </b>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Users' personal information will only be stored as long as necessary or permitted by applicable laws and regulations. Buruan will cease storing personal information as soon as it is considered that the purpose of collecting that personal information is no longer needed for business or legal purposes;
+            </li>
+            <li>
+              Personal Information collected by wahana-online may be stored, transferred, or processed by third-party service providers located either in the same country or in different countries by partners, contractors, vendors or affiliates of wahana-online. Wahana-online takes reasonable steps to ensure that all third-party service providers working with wahana-online will provide a level of protection and security comparable to Wahana's commitment under this privacy policy;
+            </li>
+            <li>
+              Wahana-online will only store and process Information while the User account is active;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Third-Party Security and Cooperation
+          </h5>
+          <ol class="space-y-1 list-[upper-alpha] pl-7">
+            <li>
+              Wahana-online does not guarantee the security of personal data and/or other information provided by Users to third parties. Wahana-online implements various security measures and levels to protect the safety of Users' data and information used on the wahana-online site. User data is stored within a highly secure network with strict special access rights to the system. If Users perform activities or access personal data, wahana-online offers the use of secure servers. All personal or sensitive information provided by Users is encrypted in the database with a high level of security as described above;
+            </li>
+            <li>
+              As the ecosystem develops and grows, Wahana may choose various forms of third-party cooperation to be linked and packaged as a single unit. Wahana-online may also participate in other relationships to offer services and/or features to wahana-online users. All third parties that collaborate have their own privacy policies and security arrangements that are independent; if a third party is affiliated with wahana-online, Wahana does not control those parties, and each has its own privacy and data collection practices separate from wahana-online. Data collected by third parties together with wahana-online, even if offered on or through the wahana-online site, may not be received by wahana-online;
+            </li>
+            <li>
+              Therefore, wahana-online is not responsible for the content, security arrangements, and activities of these related sites. These related sites are provided for Users' convenience, and Users access them at their own risk. Nevertheless, Wahana always strives to maintain integrity by accepting feedback regarding those third parties including but not limited to cases where those third parties do not function properly;
+            </li>
+          </ol>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Privacy Policy Updates
+          </h5>
+          <p>
+            Wahana-online reserves the right to update this privacy policy at any time. Therefore, wahana-online strongly recommends that all Users read carefully and periodically review each page of this privacy policy to be promptly informed of any changes to this privacy policy.
+          </p>
+        </li>
+        <li class="space-y-3">
+          <h5 class="font-semibold">
+            Questions and Complaints
+          </h5>
+          <p>
+            Any questions, issues or complaints regarding the privacy policy can be sent to support@wahana-online.com
+          </p>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
---
+++ app/components/locale/en/terms-of-services.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container space-y-8 py-16">
+    <h1 class="text-3xl text-primary text-center font-semibold">
+      Terms of Service
+    </h1>
+    <ul class="space-y-4 text-tone">
+      <li>
+        <b class="font-semibold">
+          Website User Terms
+        </b>
+        <div class="pl-6">
+          <span>
+            When visiting and using the Wahana Komputer website, including all its features and services, every User is required to comply with the following Website User Terms:
+          </span>
+          <div class="pl-8 *:-indent-8 *:[&_span]:tabular-nums">
+            <div>
+              <span>A.1</span>: Users must be at least 18 years old, previously married, or legally recognized as adults / not subject to Child Protection Law.
+            </div>
+            <div>
+              <span>A.2</span>: Access to this site is only permitted for shopping purposes and information related to this site's services.
+            </div>
+            <div>
+              <span>A.3</span>: Users are not allowed to reproduce, distribute, display, sell, rent, transmit, create derivative works from, translate, modify, reverse engineer, disassemble, decompile, or exploit the Wahana Komputer website.
+            </div>
+            <div>
+              <span>A.4</span>: Users are not allowed to upload or publish content that:
+              <div class="pl-11 *:-indent-11">
+                <div>
+                  <span>A.4.1</span>: Violates copyrights, patents, trademarks, service marks, trade secrets, or other proprietary rights.
+                </div>
+                <div>
+                  <span>A.4.2</span>: Is threatening, obscene, indecent, pornographic, or could create any civil or criminal liability under Indonesian or international law.
+                </div>
+                <div>
+                  <span>A.4.3</span>: Contains bugs, viruses, worms, trap doors, trojan horses, or other harmful code and properties.
+                </div>
+              </div>
+            </div>
+            <div>
+              <span>A.5</span>: Products offered are not manufactured by Wahana Komputer but by Vendors, except for KUBIK.
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <b class="font-semibold">
+          Intellectual Property Rights
+        </b>
+        <div class="pl-6">
+          <span>
+            Wahana Komputer is the sole owner or legal holder of all rights to the Website and its content at www.wahana-online.com. All content contained within www.wahana-online.com includes intellectual property rights protected by copyright laws and other intellectual property laws applicable worldwide. All ownership and intellectual property rights of www.wahana-online.com and its content remain with Us, our affiliates, or content licensors. All rights not expressly granted in this Agreement or by Us are protected by law, including but not limited to:
+          </span>
+          <div class="pl-8 *:-indent-8 *:[&_span]:tabular-nums">
+            <div>
+              <span>B.1</span>: Full ownership of the Software.
+            </div>
+            <div>
+              <span>B.2</span>: The Wahana Komputer name, icons, and related logos are registered trademarks in various jurisdictions and protected by copyright, trademark, or other intellectual property laws. Strictly prohibited to use, alter, or install those marks for personal gain or to discredit Wahana Komputer.
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <b class="font-semibold">
+          Links
+        </b>
+        <div class="pl-6">
+          <span>
+            This site may contain internet links to other sites owned and operated by third parties. Please note that we are not responsible for the operation or content of those sites.
+          </span>
+        </div>
+      </li>
+      <li>
+        <b class="font-semibold">
+          Transactions
+        </b>
+        <div class="pl-6">
+          <div class="pl-8 *:-indent-8 *:[&_span]:tabular-nums">
+            <div>
+              <span>C.1</span>: The website <a href="http://wahana-online.com" target="_blank" rel="noopener noreferrer" class="text-blue-500">wahana-online.com</a> or Wahana Komputer only serves as a product portfolio. For online purchases, please visit our Official Store on e-commerce or marketplaces listed on <a href="http://wahana-online.com" target="_blank" rel="noopener noreferrer" class="text-blue-500">wahana-online.com</a> or Wahana Komputer.
+            </div>
+            <div>
+              <span>C.2</span>: Some listed items are not in stock and require pre-order.
+            </div>
+            <div>
+              <span>C.3</span>: Every product purchased from Wahana Komputer's Official Store comes with a warranty period according to each manufacturer's warranty. Please contact our sales team for more information.
+            </div>
+            <div>
+              <span>C.4</span>: Items purchased cannot be returned or exchanged unless there is a prior agreement.
+            </div>
+            <div>
+              <span>C.5</span>: Every effort has been made to ensure the accuracy of all information provided. Wahana Komputer does not guarantee the accuracy of the data, including product specifications or editorial content.
+            </div>
+            <div>
+              <span>C.6</span>: If a product is damaged during the warranty period, please contact our Technical Center at (021) 6125802.
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <b class="font-semibold">
+          Product Description
+        </b>
+        <div class="pl-6">
+          <span>
+            Wahana Komputer always strives to provide product descriptions as accurately as possible. However, we do not guarantee that all descriptions or content on this website are 100% accurate, complete, up-to-date, or error-free. If a product offered by Wahana Komputer does not match the product description, you may return it in an unused condition / unopened seal.
+          </span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
