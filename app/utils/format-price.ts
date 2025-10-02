55384a222632af60b9f07581f1288d0d619b23f2
RifkyLTF <rifky.luthfan@gmail.com>
2025-10-02 08:29:46 +0700

    fix: price format

--- app/utils/format-price.ts
+++ app/utils/format-price.ts
@@ -1,4 +1,4 @@
 export default (price: string) => {
-  const formatted = `${price || 0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  const formatted = `${parseFloat(price || "0")}`.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   return `Rp${formatted}`;
 }
