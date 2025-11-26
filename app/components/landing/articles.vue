01082d9801d0292147d8f8ae37cd9cc3a35e26d9
RifkyLTF <rifky.luthfan@gmail.com>
2025-11-26 23:43:57 +0700

    style: articles on landing page

--- app/components/landing/articles.vue
+++ app/components/landing/articles.vue
@@ -3,33 +3,29 @@
     <h1 class="text-5xl text-primary font-semibold">
       {{ $t("landing.articles.title") }}
     </h1>
-    <UCarousel
-      v-slot="{ item }"
-      :items="articles"
-      class="w-full"
-      :ui="{ item: 'sm:basis-1/2 lg:basis-1/3 xl:basis-1/4' }"
-      dots
-    >
-      <NuxtLink
-        :to="$localeRoute(`/blog/${item.blog_article_id}`)"
-        class="block w-full glass border border-accent rounded-xl"
-      >
-        <img
-          :src="item.thumbnail"
-          :alt="item.title"
-          class="w-full object-cover aspect-square rounded-t-xl" loading="lazy"
-        />
-        <div class="space-y-1 p-6">
-          <h4 class="text-xl text-primary font-semibold">
-            {{ item.title }}
-          </h4>
-          <p class="text-tone line-clamp-2" v-html="item.content"></p>
-          <span class="text-sm text-primary">
-            {{ $t("landing.articles.read_more") }}
-          </span>
-        </div>
-      </NuxtLink>
-    </UCarousel>
+    <div class="flex flex-wrap justify-center gap-y-6">
+      <div v-for="article in articles" :key="article.blog_article_id" class="w-full sm:w-1/2 lg:w-1/3 px-3">
+        <NuxtLink
+          :to="$localeRoute(`/blog/${article.blog_article_id}`)"
+          class="block size-full glass border border-accent rounded-xl md:hover:scale-105 md:hover:shadow-lg transition-all"
+        >
+          <img
+            :src="article.thumbnail"
+            :alt="article.title"
+            class="w-full object-cover aspect-square rounded-t-xl" loading="lazy"
+          />
+          <div class="space-y-1 p-6">
+            <h4 class="text-xl text-primary font-semibold">
+              {{ article.title }}
+            </h4>
+            <p class="text-tone line-clamp-2" v-html="article.content"></p>
+            <span class="text-sm text-primary">
+              {{ $t("landing.articles.read_more") }}
+            </span>
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
 
     <UButton
       size="lg"
@@ -46,7 +42,7 @@ import type { BlogArticle } from "~/types/blog";
 import type { HttpSuccessWithPagination } from "~/types/http";
 
 const { data: articles } = await useFetch(
-  `${useRuntimeConfig().public.apiBase}/blog-articles?limit=5`,
+  `${useRuntimeConfig().public.apiBase}/blog-articles?limit=3`,
   {
     transform: (value: HttpSuccessWithPagination<BlogArticle[]>) => {
       return value.data.data;
