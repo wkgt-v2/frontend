40fae0d8254e643ef50d0d9e80a3ce1aea35412b
RifkyLTF <rifky.luthfan@gmail.com>
2025-12-13 23:19:05 +0700

    fix: image on initial page load

--- app/components/image.vue
+++ app/components/image.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="relative w-fit">
-    <img
-      :src="src"
-      :alt="alt"
-      :class="`${width} ${height} ${mode === 'contain' ? 'object-contain' : 'object-cover'} ${onLoad ? 'opacity-0' : ''} ${imageClasses}`"
-      loading="lazy"
-      v-on:load="onLoad = false"
-      v-on:error="isError = true;"
-    />
-    <UIcon
-      v-if="onLoad && !isError"
-      name="i-material-symbols:imagesmode"
-      class="absolute top-1/2 left-1/2 -translate-1/2 size-full animate-pulse z-10"
-    />
-    <UIcon
-      v-if="isError"
-      name="i-material-symbols:broken-image"
-      class="absolute top-1/2 left-1/2 -translate-1/2 size-full z-10"
-    />
-  </div>
+  <ClientOnly>
+    <div class="relative w-fit">
+      <img
+        :src="src"
+        :alt="alt"
+        :class="`${width} ${height} ${mode === 'contain' ? 'object-contain' : 'object-cover'} ${onLoad ? 'opacity-0' : ''} ${imageClasses}`"
+        loading="lazy"
+        v-on:load="onLoad = false"
+        v-on:error="isError = true"
+      />
+      <UIcon
+        v-if="onLoad && !isError"
+        name="i-material-symbols:imagesmode"
+        class="absolute top-1/2 left-1/2 -translate-1/2 size-full animate-pulse z-10"
+      />
+      <UIcon
+        v-if="isError"
+        name="i-material-symbols:broken-image"
+        class="absolute top-1/2 left-1/2 -translate-1/2 size-full z-10"
+      />
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
